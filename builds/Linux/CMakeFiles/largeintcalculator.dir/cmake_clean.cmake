84679e6e4b8f858fd695bf522f5d8c05641da34b
CodeByAidan <action@github.com>
2023-11-07 09:57:07 +0000

    Add build outputs

---
+++ builds/Linux/CMakeFiles/largeintcalculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/largeintcalculator.dir/largeintcalculator.c.o"
+  "CMakeFiles/largeintcalculator.dir/largeintcalculator.c.o.d"
+  "builds/Linux/Release/largeintcalculator"
+  "builds/Linux/Release/largeintcalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/largeintcalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
