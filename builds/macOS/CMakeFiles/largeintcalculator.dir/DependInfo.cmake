50aa280ba044ff244d7158bc4329a7dcc108eeb3
CodeByAidan <action@github.com>
2023-11-07 09:56:50 +0000

    Add build outputs

---
+++ builds/macOS/CMakeFiles/largeintcalculator.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/runner/work/Data-Structure-Project/Data-Structure-Project/largeintcalculator.c" "CMakeFiles/largeintcalculator.dir/largeintcalculator.c.o" "gcc" "CMakeFiles/largeintcalculator.dir/largeintcalculator.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
